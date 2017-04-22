fb2a68fcca6e70fd1259f74715838b1b90608b3b
JamesLdeng <15537773239@163.com>
2017-04-22 17:51:06 +0800

    json对象在前后端之间的传递

---
+++ WebContent/js/loanBook.js
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+
+function appendBookRow () {
+	var bookno = $("#bookno").val();
+	if (bookno == null||bookno =="") {
+		alert("请输入图示编号");
+	};
+	var rows = $("#loanTable").datagrid('getRows');
+	/*
+	 * 判断是否已经所输入编号是否已经存在在借阅列表中
+	 */
+	for (var i=0; i < rows.length; i++) {
+	  var havingBookno = rows[i]['bookno'];
+	  if (bookno == havingBookno) {
+	  	alert("该书已经在借阅列表中");
+	  	return;
+	  };
+	};
+	
+	$.post(
+		'url',
+		{bookno:bookno},
+		/*
+		 * 给表格追加行
+		 */
+		function(data){
+			$("#loanTable").datagrid('appendRow',data);
+		}
+	);
+}
+
+
